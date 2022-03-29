35db6080b091560dfea6effdaa2138fc334b3071
Omar Menjivar <omarmenjivar083@gmail.com>
2022-03-28 22:10:32 -0600

    Solucionando errores

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,7 +1,5 @@
 
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { useQuery } from "../hooks/useQuery";
 import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
 import styles from "./MovieGrid.module.css";
--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from './SearchMovie.module.css';
 import {FaSearch} from 'react-icons/fa';
 import { useNavigate } from "react-router-dom";
